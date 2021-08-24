3329bd106d992c6beec37749e7457077847a6626
HarshGarg <HarshGarg2672@gmail.com>
2021-08-24 20:02:22 +0530

    created webD modules

--- a.js
+++ a.js
@@ -2,28 +2,28 @@ let fs = require('fs');
 let path = require('path');
 let inputArr=process.argv.slice(1);
 console.log(inputArr);
-// let mainDir=inputArr[0];//webD
-// let subDirectories=inputArr.slice(1);
-// // console.log(mainDir);
-// // console.log(subDirectories);
+let mainDir=inputArr[0];//webD
+let subDirectories=inputArr.slice(1);
+// console.log(mainDir);
+// console.log(subDirectories);
 
-// let mainDirPath=path.join(process.cwd(),mainDir);
-// if(!fs.existsSync(mainDirPath)){
-//     fs.mkdirSync(mainDirPath);
-// }
-// for(let i=0;i<subDirectories.length;i++){
-//     console.log(subDirectories[i]);
-//     let folder = path.join(mainDirPath,subDirectories[i]);
-//     console.log('folder path ',folder);
-//     if(!fs.existsSync(folder)){
-//         fs.mkdirSync(folder);
-//     }
-//     for(let j=1;j<=3;j++){
-//         let module=path.join(folder,`Module${j}`);
-//         console.log('module names',module);
-//         fs.mkdirSync(module);
-//         let fileName = path.join(module,'content.md');
-//         fs.writeFileSync(fileName,'#Hello Everyone');
-//     }
-// }
+let mainDirPath=path.join(process.cwd(),mainDir);
+if(!fs.existsSync(mainDirPath)){
+    fs.mkdirSync(mainDirPath);
+}
+for(let i=0;i<subDirectories.length;i++){
+    console.log(subDirectories[i]);
+    let folder = path.join(mainDirPath,subDirectories[i]);
+    console.log('folder path ',folder);
+    if(!fs.existsSync(folder)){
+        fs.mkdirSync(folder);
+    }
+    for(let j=1;j<=3;j++){
+        let module=path.join(folder,`Module${j}`);
+        console.log('module names',module);
+        fs.mkdirSync(module);
+        let fileName = path.join(module,'content.md');
+        fs.writeFileSync(fileName,'#Hello Everyone');
+    }
+}
 
