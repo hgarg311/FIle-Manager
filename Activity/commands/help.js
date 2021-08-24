20d81a911c61e4bd3715418f79d8e4e40d2fef35
HarshGarg <HarshGarg2672@gmail.com>
2021-08-24 21:42:38 +0530

    Activity :to read files

---
+++ Activity/commands/help.js
@@ -0,0 +1,10 @@
+function helpFunction(){
+    console.log(`List of all commands
+    1. node main.js tree <pathname>
+    2. node main.js organise <pathname>
+    3. node main.js help`);
+}
+
+module.exports={
+    helpFn:helpFunction
+};
